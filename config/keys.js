8de7c47b3989e3e135aa8cbfbea92f0c40ecf3d9
moros101 <mohitgoswami16@gmail.com>
2021-11-26 10:28:50 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://admin:admin@cluster0.yikzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  pusherAppId: '1306233',
+  pusherKey: '3523a9f9ae8d2a347531',
+  pusherSecret: 'ae4c64181f8bf1fe60c6',
+  pusherCluster: 'ap2',
+  pusherEncrypted: true
+};
